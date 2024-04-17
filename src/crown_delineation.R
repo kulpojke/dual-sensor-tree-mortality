aab3a06612f8f9e8a4b9cd10a4ca8ca4244b1846
michael <michaelhuggins@protonmail.com>
2024-04-16 17:39:34 -0700

    x

--- src/crown_delineation.R
+++ src/crown_delineation.R
@@ -97,124 +97,133 @@ tiles <- list.files(path=laz_dir, pattern=pattern, full.names=TRUE)
 
 # run on each tile
 for (f in tiles) {
-  # get basename
-  base_name <- tools::file_path_sans_ext(basename(f))
+  tryCatch({
+    # get basename
+    base_name <- tools::file_path_sans_ext(basename(f))
+    print('-------------------------------------------')
+    print(paste('Processing', f, '.'))
+    print('-------------------------------------------')
 
-  print('Reading points')
-  las <- readLAS(
-    f,
-    filter='-drop_withheld',
-    select='xyzirc'
-    )
-  
-  las <- filter_duplicates(las)
-  las_check(las)
+    print('Reading points')
+    las <- readLAS(
+      f,
+      filter='-drop_withheld',
+      select='xyzirc'
+      )
+    
+    las <- filter_duplicates(las)
+    las_check(las)
 
-  # normalize and drop unwanted by height
-  print('Normalizing')
-  normed <- normalize_height(las, tin())
-  normed <- filter_poi(normed, Z >= 3 & Z <= 120)
+    # normalize and drop unwanted by height
+    print('Normalizing')
+    normed <- normalize_height(las, tin())
+    normed <- filter_poi(normed, Z >= 3 & Z <= 120)
 
-  # make chm
-  print('creating chm')
-  chm <- rasterize_canopy(
-    normed,
-    res=0.5,
-    pitfree(
-      thresholds=c(0, 10, 20),
-      max_edge=c(0, 1.5))
+    # make chm
+    print('creating chm')
+    chm <- rasterize_canopy(
+      normed,
+      res=0.5,
+      pitfree(
+        thresholds=c(0, 10, 20),
+        max_edge=c(0, 1.5))
+      )
+    
+    #  smooth
+    print('smooting chm...')
+    w <- matrix(1, 3, 3)
+    smoothed <- terra::focal(chm, w, fun=mean, na.rm=TRUE)
+    
+    smooth_chm_path <- paste0(site_dir, '/chm', '/', base_name, '.tif')
+    writeRaster(smoothed, smooth_chm_path, overwrite=TRUE)
+    
+    # find ttops with height dependent window size
+    print( 'finding ttops...')
+    
+    wf <- function(x) {
+      y <- abs(x/8)
+      y[x <= 32] <- 4
+      y[x > 80] <- 10
+      return(y)
+    }
+    
+    ttops <- locate_trees(smoothed, lmf(wf))
+    #  replace treeID with unique treeID
+    ttops$uniqueID <- mapply(generate_custom_id, utm = utm_zone, geometry = ttops$geometry)
+    
+    # segment
+    print('segmenting...')
+    segs = segment_trees(
+      normed,
+      dalponte2016(smoothed, ttops)
     )
-  
-  #  smooth
-  print('smooting chm...')
-  w <- matrix(1, 3, 3)
-  smoothed <- terra::focal(chm, w, fun=mean, na.rm=TRUE)
-  
-  smooth_chm_path <- paste0(site_dir, '/chm', '/', base_name, '.tif')
-  writeRaster(smoothed, smooth_chm_path, overwrite=TRUE)
-  
-  # find ttops with height dependent window size
-  print( 'finding ttops...')
-  
-  wf <- function(x) {
-    y <- abs(x/8)
-    y[x <= 32] <- 4
-    y[x > 80] <- 10
-    return(y)
-  }
-  
-  ttops <- locate_trees(smoothed, lmf(wf))
-  #  replace treeID with unique treeID
-  ttops$uniqueID <- mapply(generate_custom_id, utm = utm_zone, geometry = ttops$geometry)
-  
-  # segment
-  print('segmenting...')
-  segs = segment_trees(
-    normed,
-    dalponte2016(smoothed, ttops)
-  )
-  
-  print('crown metrics...')
-  crowns <- crown_metrics(
-  segs,
-  func=.stdmetrics,
-  geom='concave'
-  )
-  
-  crowns <- merge(
-    x=crowns,
-    y=st_drop_geometry(ttops)[, c("treeID", "uniqueID")],
-    by="treeID",
-    all.x=TRUE
-    )
-  
-  # filter out trees shorter than 5m
-  crowns <- filter(crowns, zq95 >= 5)
-  
-  #write vectors
-  print('writing ttops')
-  st_write(
-    ttops,
-    file.path(
-      site_dir,
-      'ttops',
-      paste0(base_name, '.gpkg')
-    )
-  )
-  
-  print('writing crowns')
-  st_write(
-    crowns,
-    file.path(
-      site_dir,
-      'crowns',
-      paste0(base_name, '.gpkg')
-    )
-  )
-
-  print('writing segmented point cloud to cold storage')
-  print(file.path(
-    cold_storage,
-    'retiled_lidar',
-    paste0(base_name, '.laz')
-  ))
-  
-  writeLAS(
+    
+    print('crown metrics...')
+    crowns <- crown_metrics(
     segs,
-    file.path(
+    func=.stdmetrics,
+    geom='concave'
+    )
+    
+    crowns <- merge(
+      x=crowns,
+      y=st_drop_geometry(ttops)[, c("treeID", "uniqueID")],
+      by="treeID",
+      all.x=TRUE
+      )
+    
+    # filter out trees shorter than 5m
+    crowns <- filter(crowns, zq95 >= 5)
+    
+    #write vectors
+    print('writing ttops')
+    st_write(
+      ttops,
+      file.path(
+        site_dir,
+        'ttops',
+        paste0(base_name, '.gpkg')
+      )
+    )
+    
+    print('writing crowns')
+    st_write(
+      crowns,
+      file.path(
+        site_dir,
+        'crowns',
+        paste0(base_name, '.gpkg')
+      )
+    )
+
+    print('writing segmented point cloud to cold storage')
+    print(file.path(
       cold_storage,
       'retiled_lidar',
       paste0(base_name, '.laz')
-      )
-  )
+    ))
+    
+    writeLAS(
+      segs,
+      file.path(
+        cold_storage,
+        'retiled_lidar',
+        paste0(base_name, '.laz')
+        )
+    )
 
-  print('moving lidar tile to cold storage')
-  
-  dst <- file.path(
-    cold_storage,
-    'segmented_lidar',
-    paste0(basename(f))
-  )
-  
-  move_file_across_drives(f, dst)
+    print('moving lidar tile to cold storage')
+    
+    dst <- file.path(
+      cold_storage,
+      'segmented_lidar',
+      paste0(basename(f))
+    )
+    
+    move_file_across_drives(f, dst)
+  }, error = function(e) {
+    print(paste('tile', f, 'is bad! It was all like,'))
+    print('- - - - - - - - - - - - - - - - - - - - - - -')
+    print(e$message)
+  })
 }
