f6db1c6b11d37ff941aa6131cccc0b7669e354f2
michael <michaelhuggins@protonmail.com>
2024-10-09 15:30:17 -0700

    debugging

--- src/start_crowns_gc.sh
+++ src/start_crowns_gc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # usage:
 
-BUCKET=$1
-UTM_ZONE=$2
+BUCKET="$1"
+UTM_ZONE="$2"
 
 # make a list of the laz files in the bucket
 gcloud storage ls gs://$BUCKET/*.laz >>  bucket_laz.list
@@ -22,15 +22,42 @@ python compare_file_lists.py \
 # above step writes a new file, todo.list
 cat todo.list | parallel \
     --bar \
-    --joblog gnu_parallel.log \
+    --joblog crowns_log.txt \
     -j 4 \
     gcloud storage cp gs://$BUCKET/{} {} && \
     Rscript crown_delineation_gc.R {} $UTM_ZONE && \
     gcloud storage cp crowns/{.}_crowns.gpkg gs://$BUCKET && \
     gcloud storage cp ttops/{.}_ttops.gpkg gs://$BUCKET && \
-    gcloud storage cp {.}_log.txt gs://$BUCKET
+    gcloud storage cp {.}_log.txt gs://$BUCKET && \
+    rm {.}_log.txt && \
+    rm ttops/{.}_ttops.gpkg && \
+    rm crowns/{.}_crowns.gpkg
 
+# when done relist the files
+rm bucket_laz.list
+rm bucket_ttops.list
+rm bucket_crowns.list
+rm todo.list
+gcloud storage ls gs://$BUCKET/*.laz >>  bucket_laz.list
+gcloud storage ls gs://$BUCKET/*_ttops.gpkg >>  bucket_ttops.list
+gcloud storage ls gs://$BUCKET/*_crowns.gpkg >>  bucket_crowns.list
 
+python compare_file_lists.py \
+    --laz_list=bucket_laz.list \
+    --ttops_list=bucket_ttops.list \
+    --crowns_list=bucket_crowns.list
 
+LINES="$(cat todo.list | wc -l)"
+
+echo "" >> crowns_log.txt
+echo "------------------------------------" >> crowns_log.txt
+echo $(date) >> crowns_log.txt
+
+if [ "$LINES" -eq "0" ]; then
+    echo "Finished: files are complete." >> crowns_log.txt
+else
+    echo "Finished: but there seem to be new laz files." >> crowns_log.txt
+    echo " Try running this again." >> crowns_log.txt
+fi
 
 
